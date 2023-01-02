bccaabe9b02dcec72bc63b2a8b734e62a53b005f
Keisuke Isaji <keisuke_isaji@mail.toyota.co.jp>
2023-01-02 19:59:30 +0900

    Initial Commit

---
+++ Groove/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Groove
+//
+//  Created by 伊佐治恵介 on 2023/01/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Groove/GrooveApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrooveApp.swift
+//  Groove
+//
+//  Created by 伊佐治恵介 on 2023/01/02.
+//
+
+import SwiftUI
+
+@main
+struct GrooveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
